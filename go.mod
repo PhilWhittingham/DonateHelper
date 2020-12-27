b6da0aa79f2f59842b438744dff7fd7f84fa0d77
Phil W <phillip.whittingham@gmail.com>
2020-12-27 17:31:31 +0000

    Added restAPI functionality for one endpoint

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/PhilWhittingham/DonateHelper
 go 1.15
 
 require (
+	github.com/labstack/echo/v4 v4.1.17 // indirect
 	github.com/urfave/cli/v2 v2.3.0 // indirect
 	go.mongodb.org/mongo-driver v1.4.4 // indirect
 	gopkg.in/gookit/color.v1 v1.1.6 // indirect
--- main.go
+++ main.go
@@ -5,24 +5,25 @@ import (
    "errors"
    "fmt"
    "log"
-   //"time"
    "os"
+   "bufio"
+   "strings"
+   "net/http"
 
+   "github.com/labstack/echo/v4"
    "github.com/urfave/cli/v2"
    "go.mongodb.org/mongo-driver/bson"
-   
    "go.mongodb.org/mongo-driver/bson/primitive"
    "go.mongodb.org/mongo-driver/mongo"
    "go.mongodb.org/mongo-driver/mongo/options"
    "gopkg.in/gookit/color.v1"
-   //"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var collection *mongo.Collection
 var ctx = context.TODO()
 
 func init() {
-    clientOptions := options.Client().ApplyURI("connect string")
+    clientOptions := options.Client().ApplyURI("connection string")
     client, err := mongo.Connect(ctx, clientOptions)
     if err != nil {
         log.Fatal(err)
@@ -37,11 +38,11 @@ func init() {
 }
 
 type Charity struct {
-    ID        primitive.ObjectID `bson:"_id"`
-    CharityID string             `bson:"charityID"`
-    CompanyID string             `bson:"companyID"`
-    Name      string             `bson:"name"`
-    Website   string             `bson:"website"`
+    ID        primitive.ObjectID `json:"_id" bson:"_id"`
+    CharityID string             `json:"charityID" bson:"charityID"`
+    CompanyID string             `json:"companyID" bson:"companyID"`
+    Name      string             `json:"name" bson:"name"`
+    Website   string             `json:"website" bson:"website"`
 }
 
 func main() {
@@ -78,7 +79,7 @@ func main() {
                     charities, err := getAll()
                     if err != nil {
                         if err == mongo.ErrNoDocuments {
-                            fmt.Print("Nothing to see here.\nRun `add 'task'` to add a task")
+                            fmt.Print("No charities are present")
                             return nil                            
                         }
 
@@ -86,6 +87,47 @@ func main() {
                     }
 
                     printCharities(charities)
+                    return nil
+                },
+            },
+            {
+                Name: "csv",
+                Aliases: []string{"c"},
+                Usage: "add charities from a csv",
+                Action: func(c *cli.Context) error {
+                    filepath := c.Args().First()
+                    if filepath == "" {
+                        return errors.New("Must contain a file path")
+                    }
+                    errList := loadCharitiesFromFile(filepath)
+                    for err := range errList {
+                        fmt.Println(err)
+                    }
+                    
+                    return nil
+                },
+            },
+            {
+                Name: "api",
+                Aliases: []string{"r"},
+                Usage: "start the rest api",
+                Action: func(c *cli.Context) error {
+                    e := echo.New()
+                    e.GET("/", func(c echo.Context) error {
+                        return c.String(http.StatusOK, "Hello, World!")
+                    })
+                    e.GET("/all", func(c echo.Context) error {
+                        charities, err := getAll()
+                        if err != nil {
+                            if err == mongo.ErrNoDocuments {
+                                fmt.Print("No charities are present")
+                                return nil                            
+                            }
+                            return err
+                        }
+                        return c.JSON(http.StatusOK, charities)
+                    })
+                    e.Logger.Fatal(e.Start(":1323"))
                     return nil
                 },
             },
@@ -102,6 +144,33 @@ func printCharities(charities []*Charity) {
     for i, v := range charities {
         color.Green.Printf("%d: %s\n", i+1, v.Name)
     }
+}
+
+func loadCharitiesFromFile(filepath string) []error {
+    inFile, err := os.Open(filepath)
+
+	if err != nil {
+		return []error{err}
+	}
+
+    defer inFile.Close()
+    errList := []error{}
+	scanner := bufio.NewScanner(inFile)
+	for scanner.Scan() {
+        s := strings.Split(scanner.Text(), ",")
+        charity := &Charity{
+            ID: primitive.NewObjectID(),
+            CharityID: s[0],
+            CompanyID: s[1],
+            Name: s[2],
+            Website: s[3],
+        }
+        if err := createCharity(charity); err != nil {
+            errList = append(errList, err)
+        }
+    }
+    
+    return errList
 }
 
 func createCharity(charity *Charity) error {
